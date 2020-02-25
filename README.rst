bb2511caa6a24dc934ab5ab910b372c271285d28
useHTML5 <mne@sait-nado.ru>
2020-02-25 11:17:41 +0500

    init

--- README.rst
+++ README.rst
@@ -1,1 +1,9 @@
 Bootstrap 4 row plugin for django-cms
+
+requirements.in:
+
+git+https://github.com/useHTML5/bootstrap4_row@0.3#egg=bootstrap4_row
+
+INSTALLED_APPS = {
+    'bootstrap4_row',
+}
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="bootstrap4_row",
-    version="0.0.2",
+    version="0.5",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
